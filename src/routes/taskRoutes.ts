ff60d95a92b3156f0b986f7777b6da7aaf297194
JomsCode21 <iramuhjsolag@gmail.com>
2026-02-19 23:32:46 +0800

    chore: some changes adding middleware

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getTask, createTask, deleteTask, updateTask } from "@/controllers/task.controller";
+import { verifyToken } from "@/middlewares/verify-token.middleware";
 
 const router = Router();
 router.get("/", getTask);
-router.post("/", createTask);
-router.delete("/:id", deleteTask);
-router.put("/:id", updateTask);
+router.post("/", verifyToken, createTask);
+router.delete("/:id", verifyToken, deleteTask);
+router.put("/:id", verifyToken, updateTask);
 
 export default router;
